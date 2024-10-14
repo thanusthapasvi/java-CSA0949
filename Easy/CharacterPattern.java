dbc96a11560072128e7cf9131676933812faa7de
thanusthapasvi <123483866+thanusthapasvi@users.noreply.github.com>
2024-10-14 08:47:53 +0530

    Add files via upload

---
+++ Easy/CharacterPattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CharacterPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Character to be printed: ");
+        char ch = sc.next().charAt(0);
+        System.out.print("Max Number of times printed: ");
+        int max = sc.nextInt();
+
+        for (int i = 1; i <= max; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+}
